1e30b2a650d9147a83a55d33882b184a09919b9f
chiri03 <lena.kiriac.97@gmail.com>
2023-05-21 15:22:57 +0300

    ex.8 task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = function (currentTime) {
+  localStorage.setItem('videoplayer-current-time', currentTime);
+};
+
+let throt_fun = throttle(onPlay, 1000);
+
+player.on('timeupdate ', throt_fun);
+
+let replayTime = localStorage.getItem('videoplayer-current-time');
+
+player
+  .setCurrentTime(replayTime)
+  .then(function () {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
